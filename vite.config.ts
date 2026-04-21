d92d603e325d0635d35c5c3c38b16ba5c8c147b2
HasibullahNaeim <hasib.naeim08@gmail.com>
2026-04-21 11:22:56 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/hasibullah-naim-folio/",
   server: {
     host: "::",
     port: 8080,
